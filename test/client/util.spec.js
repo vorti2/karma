cc82f559e578c63280a5589489939740bafb2ee5
vorti2 <development@v-software.de>
2013-11-13 20:02:29 +0100

    missing 'it' block in test

--- test/client/util.spec.js
+++ test/client/util.spec.js
@@ -4,8 +4,9 @@ var util  = require('../../client/util');
 describe('util', function() {
 
   describe('parseQueryParams', function() {
-    var params = util.parseQueryParams('?id=123&return_url=http://whatever.com');
-
-    expect(params).toEqual({id: '123', return_url: 'http://whatever.com'});
+    it("should return params in JSON object", function() {
+      var params = util.parseQueryParams('?id=123&return_url=http://whatever.com');
+      expect(params).toEqual({id: '123', return_url: 'http://whatever.com'});
+    });
   });
 });
